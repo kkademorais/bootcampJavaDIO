6d499c8a5fdae527dcf033938a03c7ac4343af54
kkademorais <kaike.carvalho@outlook.com>
2024-05-24 15:09:56 -0300

    Tipos e Variaveis classes finished

--- primeiroTeste/src/sintaxe/TiposVariaveis/TiposVariaveis.java
+++ primeiroTeste/src/sintaxe/TiposVariaveis/TiposVariaveis.java
@@ -1,6 +1,17 @@
 package sintaxe.TiposVariaveis;
 
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        System.out.println("Valor do salário mínimo desse ano = R$" + salarioMinimo);
 
-    
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        salarioMinimo = 3000;
+        System.out.println("Previsão do valor do salário mínimo para o ano q vem = R$" + salarioMinimo);
+
+        final double VALOR_PI = 3.14;
+    }
 }
